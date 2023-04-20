23162c5f3d774b13b92b05648e8fe5f4f29df2f2
Max Miliano Del Canto Quezada <milianor@localhost.localdomain>
2023-04-20 18:09:20 -0300

    update .tmux default shell, fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,8 @@ if test "$TERM" != "xterm-kitty"
   set TERM xterm-256color
 end
 
-set PATH ~/.nvm/versions/node/v18.15.0/bin $PATH
-set PATH ~/.local/share/flutter/bin $PATH
+# set PATH ~/.nvm/versions/node/v18.15.0/bin $PATH
 set PATH ~/.cargo/bin $PATH
-set PATH ~/.config/composer/vendor/bin $PATH
 
 alias ls "exa --long --icons --tree --level=1 -a"
 alias ll ls
@@ -13,9 +11,8 @@ alias python python3
 alias pip pip3
 alias pe "python3 -m venv venv"
 alias pa ". venv/bin/activate.fish"
-alias find "fdfind -H -I -i"
-alias fd "fdfind"
-alias java_set "sudo update-alternatives --config java"
+alias find "fd -H -I -i"
+# alias java_set "sudo update-alternatives --config java"
 
 function change_java
   sudo update-alternatives --config java
