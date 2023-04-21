b94e27489a891d5db4933365a8cbb6f9d4f373ae
Max Miliano <milianordelcanto@gmail.com>
2023-04-20 23:53:47 -0300

    hyprland, eww

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def draw_table(stdscr, options):
                 cursor_y += 1
                 stdscr.move(cursor_y, x)
                 stdscr.refresh()
-        elif key == ord('\n'):
+        elif key == ord("\n"):
             # User has made a selection
             selection = options[cursor_y - 2]
             return selection
@@ -55,12 +55,14 @@ def draw_table(stdscr, options):
 
 
 def main(stdscr):
-    options = ["tmux", "kitty", "fish"]
+    options = ["tmux", "kitty", "fish", "hyprland", "eww"]
 
     config_paths = {
         "tmux": os.path.expanduser("~/.tmux.conf"),
         "kitty": os.path.expanduser("~/.config/kitty"),
-        "fish": os.path.expanduser("~/.config/fish")
+        "fish": os.path.expanduser("~/.config/fish"),
+        "hyprland": os.path.expanduser("~/.config/hypr"),
+        "eww": os.path.expanduser("~/.config/eww"),
     }
 
     # Draw the table and get the user's selection
@@ -70,34 +72,46 @@ def main(stdscr):
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Delete the old configuration file or directory, if it exists
-    if user_selection == 'tmux':
+    if user_selection == "tmux":
         # Construct the path to the file you want to delete
-        file_path = os.path.join(script_dir, '.tmux.conf')
+        file_path = os.path.join(script_dir, ".tmux.conf")
 
         if os.path.exists(file_path):
             os.remove(file_path)
 
-    elif user_selection == 'kitty':
+    elif user_selection == "kitty":
         # Construct the path to the directory you want to delete
-        dir_path = os.path.join(script_dir, '.config/kitty')
+        dir_path = os.path.join(script_dir, ".config/kitty")
 
         if os.path.exists(dir_path):
             shutil.rmtree(dir_path)
 
-    elif user_selection == 'fish':
+    elif user_selection == "fish":
         # Construct the path to the directory you want to delete
-        dir_path = os.path.join(script_dir, '.config/fish')
+        dir_path = os.path.join(script_dir, ".config/fish")
+
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+    elif user_selection == "hyprland":
+        # Construct the path to the directory you want to delete
+        dir_path = os.path.join(script_dir, ".config/hyprland")
+
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+    elif user_selection == "eww":
+        # Construct the path to the directory you want to delete
+        dir_path = os.path.join(script_dir, ".config/eww")
 
         if os.path.exists(dir_path):
             shutil.rmtree(dir_path)
 
     # Copy the new configuration file or directory
-    if user_selection == 'tmux':
-        subprocess.run(["cp", "-r", config_paths[user_selection],
-                        script_dir])
+    if user_selection == "tmux":
+        subprocess.run(["cp", "-r", config_paths[user_selection], script_dir])
     else:
-        subprocess.run(["cp", "-r", config_paths[user_selection],
-                        script_dir + "/.config"])
+        subprocess.run(
+            ["cp", "-r", config_paths[user_selection], script_dir + "/.config"]
+        )
 
 
 curses.wrapper(main)
