0f1317d8b0a8dec888a0cdb9650083d516af74ef
Max Miliano <milianordelcanto@gmail.com>
2023-04-28 00:15:46 -0300

    update nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,18 @@ return {
     'maxmx03/dracula.nvim',
     lazy = false,
     priority = 1000,
+    branch = 'draculapro',
     config = function()
       local dracula = require 'dracula'
+      local draculapro = require 'draculapro'
+
+      draculapro.setup {
+        theme = 'pro',
+      }
 
       dracula.setup {
+        colors = draculapro.colors,
+        dracula_pro = draculapro,
         override = function(c)
           return {
             CmpItemKindText = { reverse = true },
@@ -34,18 +42,20 @@ return {
             CmpItemKindEvent = { reverse = true },
             CmpItemKindOperator = { reverse = true },
             CmpItemKindTypeParameter = { reverse = true },
-            TelescopeResultsBorder = { fg = c.float_bg, bg = c.float_bg },
-            TelescopeResultsNormal = { bg = c.float_bg },
+            TelescopeResultsBorder = { fg = c.bgdark, bg = c.bgdark },
+            TelescopeResultsNormal = { bg = c.bgdark },
             TelescopePreviewNormal = { bg = c.bg },
-            TelescopePromptBorder = { fg = c.float_bg, bg = c.float_bg },
-            TelescopeTitle = { fg = c.fg, bg = c.comment },
+            TelescopePromptBorder = { fg = c.bgdark, bg = c.bgdark },
             TelescopePromptPrefix = { fg = c.purple },
-            CmpItemKindBorder = { fg = c.float_bg, bg = c.float_bg },
+            CmpItemKindBorder = { fg = c.bgdark, bg = c.bgdark },
           }
         end,
       }
 
       vim.cmd.colorscheme 'dracula'
     end,
+    dependencies = {
+      'maxmx03/draculapro',
+    },
   },
 }
