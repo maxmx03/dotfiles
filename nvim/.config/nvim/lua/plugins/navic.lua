19a7c68eb2e94417185d6e7eb71ab041da1c8ab9
Max Miliano <milianordelcanto@gmail.com>
2023-05-07 12:15:00 -0300

    fix navic deprecated icons

--- nvim/.config/nvim/lua/plugins/navic.lua
+++ nvim/.config/nvim/lua/plugins/navic.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       local navic = require 'nvim-navic'
       local breadcrumbs = require 'core.navic.breadcrumbs'
+      local icons = require 'core.navic.icons'.kind
 
       breadcrumbs.create_winbar()
 
@@ -11,6 +12,42 @@ return {
         highlight = true,
         depth_limit = 0,
         depth_limit_indicator = '..',
+        icons = {
+          Array = icons.Array .. ' ',
+          Boolean = icons.Boolean,
+          Class = icons.Class .. ' ',
+          Color = icons.Color .. ' ',
+          Constant = icons.Constant .. ' ',
+          Constructor = icons.Constructor .. ' ',
+          Enum = icons.Enum .. ' ',
+          EnumMember = icons.EnumMember .. ' ',
+          Event = icons.Event .. ' ',
+          Field = icons.Field .. ' ',
+          File = icons.File .. ' ',
+          Folder = icons.Folder .. ' ',
+          Function = icons.Function .. ' ',
+          Interface = icons.Interface .. ' ',
+          Key = icons.Key .. ' ',
+          Keyword = icons.Keyword .. ' ',
+          Method = icons.Method .. ' ',
+          Module = icons.Module .. ' ',
+          Namespace = icons.Namespace .. ' ',
+          Null = icons.Null .. ' ',
+          Number = icons.Number .. ' ',
+          Object = icons.Object .. ' ',
+          Operator = icons.Operator .. ' ',
+          Package = icons.Package .. ' ',
+          Property = icons.Property .. ' ',
+          Reference = icons.Reference .. ' ',
+          Snippet = icons.Snippet .. ' ',
+          String = icons.String .. ' ',
+          Struct = icons.Struct .. ' ',
+          Text = icons.Text .. ' ',
+          TypeParameter = icons.TypeParameter .. ' ',
+          Unit = icons.Unit .. ' ',
+          Value = icons.Value .. ' ',
+          Variable = icons.Variable .. ' ',
+        },
       }
     end,
   },
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     config = function()
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'markdown' },
+        ensure_installed = { 'markdown', 'markdown_inline' },
         auto_install = true,
         highlight = {
           enable = true,
