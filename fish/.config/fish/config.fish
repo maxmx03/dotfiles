0a5ece6fb097ae1ade48d94b76a9ef68f1454aaf
Max Miliano <milianordelcanto@gmail.com>
2023-06-22 23:16:34 -0300

    update: fish neovim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,7 @@ set TERM xterm-256color
 set PATH ~/.cargo/bin $PATH
 set PATH /usr/bin $PATH
 set PATH ~/go/bin $PATH
-set PATH ~/.nvm/versions/node/v18.16.0/bin $PATH
+set PATH ~/.nvm/versions/node/v18.16.1/bin $PATH
 
 alias ls "exa --long --icons --tree --level=1 -a"
 alias ll ls
@@ -11,16 +11,8 @@ alias python python3
 # alias pip pip3
 alias pe "python3 -m venv venv"
 alias pa ". venv/bin/activate.fish"
-alias find "fd -H -I -i"
+alias find "fdfind -H -I -i"
 
 function fish_greeting
   neofetch
-end
-
-function notes
- open https://maxmx03.github.io/notes/
-end
-
-function milianor
-  NVIM_APPNAME=milianor-vim nvim $argv
 end
--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,20 +1,11 @@
 return {
   {
-    'toppair/peek.nvim',
-    build = 'deno task --quiet build:fast',
+    'iamcco/markdown-preview.nvim',
+    build = 'cd app && npm install',
     ft = { 'markdown' },
     config = function()
-      require('peek').setup {
-        auto_load = true,        -- whether to automatically load preview when
-        close_on_bdelete = true, -- close preview window on buffer delete
-        syntax = true,           -- enable syntax highlighting, affects performance
-        theme = 'dark',          -- 'dark' or 'light'
-        update_on_change = true,
-        app = 'browser', -- 'webview', 'browser', string or a table of strings
-        filetype = { 'markdown' }, -- list of filetypes to recognize as markdown
-        throttle_at = 200000,   -- start throttling when file exceeds this
-        throttle_time = 'auto', -- minimum amount of time in milliseconds
-      }
+      vim.g.mkdp_theme = 'dark'
+      vim.g.mkdp_filetypes = { 'markdown' }
     end,
   },
 }
