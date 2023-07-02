884677fbf4e392dad0e9dcf920b40538a0e95d23
Max Miliano <milianordelcanto@gmail.com>
2023-07-02 09:33:55 -0300

    set shell for windows

--- nvim/.config/nvim/lua/milianor/options.lua
+++ nvim/.config/nvim/lua/milianor/options.lua
@@ -43,4 +43,15 @@ vim.opt.spell = false
 vim.opt.spelllang = 'en'
 vim.opt.scrolloff = 8 -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.shell = 'fish'
+
+if vim.fn.has('win64') then
+  vim.cmd [[
+    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+    let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+    let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+    set shellquote= shellxquote=
+  ]]
+else
+  vim.opt.shell = 'fish'
+end
