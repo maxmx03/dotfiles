1e5deacaba947ceb426fb6e959cc74a48d471172
Max Del Canto <milianordelcanto@gmail.com>
2023-11-29 16:11:11 -0300

    refactor:  fish, tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,7 @@ set PATH ~/.cargo/bin $PATH
 set PATH /usr/bin $PATH
 set PATH ~/go/bin $PATH
 set PATH ~/.local/share/flutter/bin $PATH
-set PATH ~/.nvm/versions/node/v18.18.0/bin $PATH
+set PATH ~/.nvm/versions/node/v20.10.0/bin $PATH
 
 alias ls "exa --long --icons --tree --level=1 -a"
 alias ll ls
@@ -13,7 +13,6 @@ alias python python3
 alias pe "python3 -m venv venv"
 alias pa ". venv/bin/activate.fish"
 alias find "fdfind -H -I -i"
-alias lua "luajit"
 
 function fish_greeting
   neofetch
