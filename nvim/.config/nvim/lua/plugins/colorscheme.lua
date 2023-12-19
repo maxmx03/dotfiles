d4f38d9a62b93be2ca63f11f7c1fd122f01ac250
Max Miliano <milianordelcanto@gmail.com>
2023-12-19 16:50:23 -0300

    feat: neovim dashboard, hyprland cmus keybinds

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,12 @@ return {
       require('solarized').setup {
         theme = 'neo',
         transparent = true,
+        highlights = {
+          DashboardHeader = { fg = '#839496' },
+          DashboardFooter = { fg = '#586e75' },
+          DashboardKey = { fg = '#2aa198'},
+          Directory = { fg = '#268bd2' },
+        },
       }
 
       vim.cmd.colorscheme 'solarized'
