28c56496a771c6df4061497af873b62940dd9439
Max Miliano <milianordelcanto@gmail.com>
2023-12-21 14:09:18 -0300

    fix: bufferline,colorscheme

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -2,6 +2,7 @@ return {
   {
     'akinsho/bufferline.nvim',
     version = '*',
+    event = 'VeryLazy',
     config = function()
       require('bufferline').setup {
         options = {
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
   {
     'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
     config = function()
       require('solarized').setup {
         theme = 'neo',
