a24c0f262d18bbf2d0297eb286e33c0cc7372c14
Max Miliano <milianordelcanto@gmail.com>
2023-12-31 09:02:46 -0300

    refactor: switch_bg

--- waybar/.config/waybar/scripts/switch_bg.py
+++ waybar/.config/waybar/scripts/switch_bg.py
@@ -1,42 +1,48 @@
-import subprocess
-import re
 import os
+import random
+import subprocess
+
+hyprland_folder = "/usr/share/hyprland"
+home_dir = os.path.expanduser("~")
+wallpaper_folder = os.path.join(home_dir, "Wallpapers")
 
 
-def is_type(value, is_type):
-    return type(value) == is_type or isinstance(value, is_type)
+def switch_wallpaper(path):
+    cmd = f"swww img {path} --transition-fps 60 --transition-type wipe --transition-duration 3"
+    subprocess.run(
+        cmd,
+        shell=True,
+    )
+
+
+def get_random_img_file(path: str) -> str:
+    png_files = [
+        file
+        for file in os.listdir(path)
+        if file.lower().endswith(".png")
+        or file.lower().endswith(".jpg")
+        or file.lower().endswith(".webp")
+    ]
+
+    if png_files:
+        return os.path.join(path, random.choice(png_files))
+    else:
+        return ""
 
 
 basename = os.path.basename
 result = subprocess.run("swww query", shell=True, capture_output=True, text=True)
-actual_wallpaper = re.findall(r"(\/.*?\.[\w:]+)", result.stdout)
+actual_wallpaper = basename(result.stdout).replace("\n", "")
 
 changed_wallpaper = False
 
 while not changed_wallpaper:
-    new_wallpaper = subprocess.run(
-        "find ~/Wallpapers/ -type f | shuf --random-source=/dev/urandom -n 1",
-        shell=True,
-        capture_output=True,
-        text=True,
-    ).stdout.replace("\n", "")
+    new_wallpaper = get_random_img_file(path=wallpaper_folder)
 
-    if is_type(value=actual_wallpaper, is_type=list):
-        if basename(actual_wallpaper[0]) != basename(new_wallpaper):
-            subprocess.run(
-                f"swww img '{new_wallpaper}' --transition-fps 60 --transition-type wipe --transition-duration 3",
-                shell=True,
-            )
-            changed_wallpaper = True
-        else:
-            subprocess.run(
-                "swww img /usr/share/hyprland/wall_anime_2K.png",
-                shell=True,
-            )
-            break
+    if actual_wallpaper != basename(new_wallpaper):
+        switch_wallpaper(path=new_wallpaper)
+        changed_wallpaper = True
     else:
-        subprocess.run(
-            "swww img /usr/share/hyprland/wall_anime_2K.png",
-            shell=True,
-        )
-        break
+        hyprland_wallpaper = get_random_img_file(path=hyprland_folder)
+        switch_wallpaper(path=hyprland_wallpaper)
+        changed_wallpaper = True
