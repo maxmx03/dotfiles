5da880c9dfb467af2e23ad447a646c3784b71d14
Max Miliano <milianordelcanto@gmail.com>
2024-01-04 14:21:38 -0300

    feat: dots remove, dots list

--- dots.zsh
+++ dots.zsh
@@ -1,12 +1,51 @@
 #!/usr/bin/env zsh
 
 export DOTS_DIR="${XDG_DATA_HOME:-$HOME}/dotfiles"
+export DOTS_CONFIG_FILE="$DOTS_DIR/dots_configs.txt"
 default_msg="⚡ Dots"
+
+function register_config() {
+    if [ ! -e "$DOTS_CONFIG_FILE" ]; then
+        touch "$DOTS_CONFIG_FILE"
+    fi
+    config=$(grep -w "$1" "$DOTS_CONFIG_FILE")
+    if [ -z "$config" ]; then
+        echo "$1" >> "$DOTS_CONFIG_FILE"
+    fi
+}
+
+function _dots_remove() {
+    echo "$default_msg - remove"
+    pushd -q "$HOME"
+    config=$(fd -H -I --maxdepth 2 -E .cache -E .local -E dotfiles "^$1$")
+
+    echo "$default_msg - The following commands are going to be executed:"
+    echo "sed -i 's/$1//g' $DOTS_CONFIG_FILE"
+    echo "rm -rf $config"
+    echo "Proceed with these commands? (y/n): "
+    read answer
+
+    if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
+        sed -i "s/$1//g" $DOTS_CONFIG_FILE
+        rm -rf $config
+    else
+        echo "$default_msg - Operation canceled."
+    fi
+    popd -q
+}
+
+function _dots_list() {
+    if [ ! -e "$DOTS_CONFIG_FILE" ]; then
+        touch "$DOTS_CONFIG_FILE"
+    fi
+    echo "$default_msg - list"
+    cat "$DOTS_CONFIG_FILE"
+}
 
 function _dots_install() {
     echo "$default_msg - Install"
     pushd -q "$DOTS_DIR"
-    config=$(fd -H -I --maxdepth 2 "^$1$")
+    config=$(fd -H -I --maxdepth 2 -E .local -E .cache "^$1$")
 
     echo "$default_msg - The following commands are going to be executed:"
     echo "cp -r $DOTS_DIR/$config $HOME/$config"
@@ -16,6 +55,7 @@ function _dots_install() {
         if [ -e "$config" ] && [ ! -e "$HOME/$config" ]; then
             cp -r "$DOTS_DIR/$config" "$HOME/$config"
             echo "$default_msg - $config was installed"
+            register_config "$1"
         else
             echo "$default_msg - '$config' not found or already exists"
         fi
@@ -34,7 +74,9 @@ Usage: dots <command> [options]
 COMMANDS:
     help           Show this help message
     install        Install config
-    update         Update dots"
+    update         Update dots
+    remove         Remove dots
+    list           List dots"
 }
 
 function _dots_update() {
@@ -68,6 +110,8 @@ function dots() {
         help "_dots_help"
         update "_dots_update"
         install "_dots_install"
+        list "_dots_list"
+        remove "_dots_remove"
     )
 
     emulate -L zsh
