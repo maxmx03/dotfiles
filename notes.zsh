7adb10702e3e2e0cf624d78f1bd2e900f4b4fc39
Max Miliano <milianordelcanto@gmail.com>
2024-01-15 21:58:41 -0300

    fix: notes.zsh
    fix the commands write and remove

--- notes.zsh
+++ notes.zsh
@@ -10,17 +10,19 @@ if [ ! -e "$NOTES_DIR" ]; then
 fi
 
 function _nt_register() {
-	local note=$(grep -w "$1" "$NOTES_NOTE_LIST")
+	local note=$(grep -i -x "$1" "$NOTES_NOTE_LIST")
+	echo "$note to register"
+	sleep 5
 	if [ -z "$note" ]; then
 		echo "$1" >> "$NOTES_NOTE_LIST"
 	fi
 }
 
 function _nt_unregister() {
-		 local sanitized_path=$(echo "$1" | sed 's/[^^]/[&]/g; s/\^/\\^/g')
+	local sanitized_path=$(echo "$1" | sed 's/[^^]/[&]/g; s/\^/\\^/g')
 
-    sed -i "\|$sanitized_path|d" "$NOTES_NOTE_LIST"
-    sed -i '/^$/d' "$NOTES_NOTE_LIST"
+	sed -i "\|$sanitized_path|d" "$NOTES_NOTE_LIST"
+	sed -i '/^$/d' "$NOTES_NOTE_LIST"
 }
 
 function _nt_read() {
@@ -28,27 +30,35 @@ function _nt_read() {
 }
 
 function _nt_write() {
-	local user_path="$NOTES_DIR/$1"
-	local content=$(basename $user_path)
+	local file="$1"
+	local file_path="$NOTES_DIR/$file"
+	local content=$(basename $file_path)
+	local file_from_list=$(grep -i -x $1 $NOTES_NOTE_LIST)
 
-	if [[ "$content" == *.md ]]; then
-		directory_path="${user_path%/*}"
+	if [ ! -z "$file_from_list" ]; then
+		vim $NOTES_DIR/$file_from_list
+	else
+		if [[ "$content" == *.md ]]; then
+			directory_path="${file_path%/*}"
 
-		if [ ! -e "$directory_path" ]; then
-			mkdir -p "$directory_path"
+			if [ ! -e "$directory_path" ]; then
+				mkdir -p "$directory_path"
+			fi
+
+			if [ ! -e "$file_path" ]; then
+				touch $file_path
+				content="# Title: $content\n\n"
+				content+="Date: $(date +"%Y-%m-%d %H:%M:%S")\\ \n"
+				content+="Author: $USER\n\n"
+				content+="---\n"
+				echo "$content" >> "$file_path"
+				echo "register $file"
+				sleep 5
+				_nt_register $file
+			fi
+
+			$EDITOR $file_path
 		fi
-
-		if [ ! -e "$user_path" ]; then
-      touch $user_path
-      content="# Title: $content\n\n"
-      content+="Date: $(date +"%Y-%m-%d %H:%M:%S")\\ \n"
-      content+="Author: $USER\n\n"
-      content+="---\n"
-			echo "$content" >> "$user_path"
-		  _nt_register $user_path
-		fi
-
-		$EDITOR $user_path
 	fi
 }
 
@@ -57,7 +67,7 @@ function _nt_add() {
 		echo "You need to pass the folder that have all your notes."
 		echo "Example: nt add mynotes"
 	else
-	  pushd -q "$1"
+		pushd -q "$1"
 		cp -r * "$NOTES_DIR"
 		echo "$(fd md)" >> "$NOTES_NOTE_LIST"
 		popd -q
@@ -66,7 +76,7 @@ function _nt_add() {
 
 function _nt_remove() {
 	pushd -q "$NOTES_DIR"
-	local note=$(fd -p -g "**/$1")
+	local note=$(grep -i -x $1 $NOTES_NOTE_LIST)
 
 	if [ -e "$note" ]; then
 		echo "rm -rf $NOTES_DIR/$note"
@@ -75,7 +85,7 @@ function _nt_remove() {
 
 		if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
 			rm -rf "$note"
-	    _nt_unregister "$NOTES_DIR/$note"
+			_nt_unregister "$note"
 		else
 			echo "operation canceled"
 		fi
@@ -87,37 +97,37 @@ function _nt_remove() {
 }
 
 function _nt_list() {
-    if [ ! -e "$NOTES_NOTE_LIST" ]; then
-        touch "$NOTES_NOTE_LIST"
-    fi
-    cat "$NOTES_NOTE_LIST"
+	if [ ! -e "$NOTES_NOTE_LIST" ]; then
+		touch "$NOTES_NOTE_LIST"
+	fi
+	cat "$NOTES_NOTE_LIST"
 }
 
 function _nt_help() {
-    echo "📝 Notes - Help
+	echo "📝 Notes - Help
 
-Usage: nt <command> [options]
-Dependencies: charmbracelet/glow, sharkdp/fd
+	Usage: nt <command> [options]
+	Dependencies: charmbracelet/glow, sharkdp/fd
 
-COMMANDS:
-    help           Show this help message
-    list           list notes
-    read           read note
-    write          write note
-		add            add all your notes to the notes folder
-    remove         remove note"
+	COMMANDS:
+	help           Show this help message
+	list           list notes
+	read           read note
+	write          write note
+	add            add all your notes to the notes folder
+	remove         remove note"
 }
 
 function nt() {
 	typeset -A subcmds=(
-    help "_nt_help"
-		read "_nt_read"
-		write "_nt_write"
-		add "_nt_add"
-		remove "_nt_remove"
-		list "_nt_list"
-  )
+	help "_nt_help"
+	read "_nt_read"
+	write "_nt_write"
+	add "_nt_add"
+	remove "_nt_remove"
+	list "_nt_list"
+)
 
-	emulate -L zsh
-  [[ -z "$subcmds[$1]" ]] && { _nt_help; return 1 } || ${subcmds[$1]} $2
+emulate -L zsh
+[[ -z "$subcmds[$1]" ]] && { _nt_help; return 1 } || ${subcmds[$1]} $2
 }
