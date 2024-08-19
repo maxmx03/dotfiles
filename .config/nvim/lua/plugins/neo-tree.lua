18e6c50453e5c4d4cef29381043edf5c33587ed7
Max Miliano <milianordelcanto@gmail.com>
2024-08-18 23:15:15 -0300

    refactor: neo-tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,6 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
-  commit = '206241e451c12f78969ff5ae53af45616ffc9b72',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
