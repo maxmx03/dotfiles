dc523cf7068ca99ed5421ce12f0815dbc007be87
Max Miliano <milianordelcanto@gmail.com>
2024-09-03 19:59:32 -0300

    refactor: tokyonight

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -16,7 +16,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      local transparent = false
+      local transparent = true
       require('tokyonight').setup {
         transparent = transparent,
         style = 'night',
@@ -27,20 +27,22 @@ return {
           group.LspInlayHint = { fg = darken(c.blue1, 0.7), bg = 'NONE' }
           group['@lsp.typemod.function.defaultLibrary'] = { link = 'Function' }
           group['HopNextKey'] = { fg = c.orange }
-          group['PmenuThumb'] = { bg = c.purple }
-          group['WinSeparator'] = { fg = c.blue }
-          group['WhichKeyBorder'] = { link = 'WinSeparator' }
-          group['CursorLineNr'] = { fg = c.green1 }
+          group['PmenuThumb'] = { bg = c.orange }
+          group['Pmenu'] = { bg = 'NONE' }
+          group['PmenuSel'] = { fg = c.orange, reverse = true }
+          group['WinSeparator'] = { fg = c.border_highlight }
+          -- group['WhichKeyBorder'] = { link = 'WinSeparator' }
+          -- group['CursorLineNr'] = { fg = c.green1 }
           dashboard(group, 'Header', { fg = c.purple })
           dashboard(group, 'Desc', { fg = c.fg })
           dashboard(group, 'Icon', { link = 'Directory' })
           dashboard(group, 'Key', { fg = c.green1 })
           dashboard(group, 'Footer', { fg = c.comment })
           telescope(group, 'Border', { fg = c.blue, bg = c.bg_float })
-          telescope(group, 'PromptTitle', { fg = c.blue, reverse = true })
-          telescope(group, 'ResultsTitle', { fg = c.blue, reverse = true })
-          telescope(group, 'PreviewTitle', { fg = c.blue, reverse = true })
-          telescope(group, 'SelectionCaret', { fg = c.purple })
+          telescope(group, 'PromptTitle', { fg = c.orange, reverse = true })
+          telescope(group, 'ResultsTitle', { fg = c.green1, reverse = true })
+          telescope(group, 'PreviewTitle', { fg = c.green1, reverse = true })
+          telescope(group, 'SelectionCaret', { fg = c.orange, bg = c.bg_highlight })
 
           if transparent then
             bufferline(group, 'Fill', { bg = c.bg })
