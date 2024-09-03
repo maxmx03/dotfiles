1ce6dd394e16495d5ba05a6d3f886b1b3b58c164
Max Miliano <milianordelcanto@gmail.com>
2024-09-03 19:35:49 -0300

    fix: wallpaper.sh

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -4,7 +4,7 @@ export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 declare wallpapers wallpaper total_wallpapers random_index
 
-wallpapers=($HOME/Wallpapers/*)
+wallpapers=($HOME/wallpapers/*)
 total_wallpapers=${#wallpapers[@]}
 random_index=$((RANDOM % "$total_wallpapers"))
 wallpaper=${wallpapers["$random_index"]}
