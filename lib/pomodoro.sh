88a4b762c5ce802b93b3c57d50154b81507d4c04
Max Miliano <milianordelcanto@gmail.com>
2024-09-28 10:45:09 -0300

    refactor: pomodoro

--- lib/pomodoro.sh
+++ lib/pomodoro.sh
@@ -1,15 +1,70 @@
 #!/bin/bash
 
+function _timer {
+  declare -i t percentage
+  local message
+  t=$(($1 * 60))
+  message=$2
+  for ((i = 1; i <= t; i++)); do
+    percentage=$((i * 100 / t))
+    bar_length=$((percentage / 2))
+    printf "\r$message $((t - i))s [%-${bar_length}s] %d%%" "$(seq -s "#" "$bar_length" | tr -d '[:digit:]')" "$percentage"
+    sleep 1s
+  done
+
+  echo -e "\nDone!"
+}
+
 function pomodoro {
-  declare -A pomo_options
-  pomo_options["work"]="45"
-  pomo_options["break"]="15"
-  if [ -n "$1" -a -n "${pomo_options["$1"]}" ]; then
-  val=$1
-  echo "$val"
-  cmus-remote -p
-  timer ${pomo_options["$val"]}m
-  cmus-remote -s
-  notify-send -t 8000 "$val" "session done"
-  fi
+  local state=$1
+  local sound=/usr/share/sounds/freedesktop/stereo/complete.oga
+
+  for ((session = 1; ; session++)); do
+    case $state in
+    work)
+      _timer "25" "work"
+      echo "pw-cat -p $sound" | at now
+      notify-send -t 8000 "work" "session done"
+      ;;
+    break)
+      _timer "5" "break"
+      notify-send -t 8000 "break" "session done"
+      echo "pw-cat -p $sound" | at now
+      ;;
+    extrabreak)
+      _timer "10" "extra break"
+      notify-send -t 8000 "long break" "session done"
+      echo "pw-cat -p $sound" | at now
+      ;;
+    *)
+      echo "
+        usage:
+        pomodoro work
+        pomodoro break
+        "
+      ;;
+    esac
+
+    case $((session % 4)) in
+    0)
+      state="extrabreak"
+      ;;
+    *)
+      case $state in
+      work)
+        state="break"
+        ;;
+      break)
+        state="work"
+        ;;
+      esac
+      ;;
+    esac
+    read -p "Continue to the next session? [y/N]: " confirm
+    if [[ $confirm != [yY] ]]; then
+      echo "Pomodoro finalized."
+      break
+    fi
+    clear
+  done
 }
