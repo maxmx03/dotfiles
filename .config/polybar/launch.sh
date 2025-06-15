61b0aa240f14d0a501595306d09aa72d562bc529
Max Miliano <milianordelcanto@gmail.com>
2025-06-15 20:17:21 -0300

    fix(polybar): wait until amixer and weather are ready

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,18 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+# wait until amixer is ready
+while [[ -z $(amixer scontrols) ]]; do
+  sleep 1
+done
+
+# wait until openmeteo is ready
+while [[ -z $("$HOME"/.config/polybar/scripts/openmeteo) ]]; do
+  sleep 1
+done
+
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log
 polybar example 2>&1 | tee -a /tmp/polybar1.log &
 disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log &
