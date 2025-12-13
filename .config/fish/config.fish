bf10b607b0ac249f10681c40483e5f92a7c0c71a
Max Miliano <milianordelcanto@gmail.com>
2025-12-13 12:42:41 -0300

    chore: fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,30 @@
-alias ls="eza --icons"
-alias ll="eza --long --icons -a"
-
-bind \cf fzf
-
+# @fish-lsp-disable 2002 2003
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if not functions -q fundle;
-  eval (wget https://git.io/fundle -O ~/.config/fish/functions/fundle.fish);
-else
-  fundle plugin 'jethrokuan/z'
-  fundle plugin joseluisq/gitnow
-  fundle plugin franciscolourenco/done
-
-  set -U __done_notify_sound 1
-
-  fundle init
+if not functions -q fundle
+    eval (wget https://git.io/fundle -O ~/.config/fish/functions/fundle.fish)
+    return false
 end
+
+fundle plugin jethrokuan/z
+fundle plugin franciscolourenco/done
+fundle init
+
+function fish_greeting
+    if test $TERM = xterm-ghostty
+        fastfetch
+    end
+end
+
+alias ls="eza --icons"
+alias ll="eza --long --icons -a"
+set -U __done_notify_sound 1
+set -U __done_notification_urgency_level low
+set FNM_PATH "$HOME/.local/share/fnm"
+set CARGO_PATH "$HOME/.cargo/bin"
+set BIN_PATH "$HOME/.local/bin"
+set -gx PATH $BIN_PATH $FNM_PATH $CARGO_PATH $PATH
+set -gx EDITOR nvim
+eval (fnm env)
