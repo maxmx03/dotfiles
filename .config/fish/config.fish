90ef15a0683d90787c523c2ec70b7ecf47256044
Max Miliano <milianordelcanto@gmail.com>
2026-02-09 19:38:02 -0300

    refactor(fish): flutter path and greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,19 +13,21 @@ fundle plugin franciscolourenco/done
 fundle init
 
 function fish_greeting
-    if test $TERM = xterm-256color
-        # fastfetch
-    end
+    # if test $TERM = xterm-256color
+    # fastfetch
+    # end
 end
 
 alias ls="eza --icons"
 alias ll="eza --long --icons -a"
+alias lz="eza --long --icons -a --total-size"
 set -U __done_notify_sound 1
 set -U __done_notification_urgency_level low
 # fish_add_path "$HOME/.local/share/fnm"
 fish_add_path "$HOME/Android/Sdk/platform-tools"
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/.cargo/bin"
-fish_add_path "$HOME/develop/flutter_linux_3.38.5-stable/flutter/bin"
+fish_add_path "$HOME/develop/flutter/bin"
+
 set -gx EDITOR nvim
 # eval (fnm env)
