2a81accf894132dac2641b1575db9f3ff53e6a8a
Max Miliano <milianordelcanto@gmail.com>
2026-04-02 14:35:09 -0300

    feat(modules): emacs removed

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-function change_bg() {
-	wallpaper_dir="$(xdg-user-dir PICTURES)/wallpapers"
-	wallpapers=("$wallpaper_dir"/*)
-	total_wallpapers=${#wallpapers[@]}
-	random_index=$((RANDOM % "$total_wallpapers"))
-	wallpaper=${wallpapers["$random_index"]}
-	bg_info=$(awww query)
-	current_wallpaper="${bg_info##*:}"
-	awww img "$wallpaper" --transition-fps 60 --transition-type wipe --transition-duration 3
-}
-
-change_bg
+wallpaper_dir="$(xdg-user-dir PICTURES)/wallpapers"
+wallpapers=("$wallpaper_dir"/*)
+total_wallpapers=${#wallpapers[@]}
+random_index=$((RANDOM % "$total_wallpapers"))
+wallpaper=${wallpapers["$random_index"]}
+bg_info=$(awww query)
+current_wallpaper="${bg_info##*:}"
+awww img "$wallpaper" --transition-fps 60 --transition-type wipe --transition-duration 3
