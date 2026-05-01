19997a8695b9e7a0a5d68f6ee823e16c068d1555
Max Miliano <milianordelcanto@gmail.com>
2026-05-01 06:53:22 -0300

    fix: change wallpaper script

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 wallpaper_dir="$(xdg-user-dir PICTURES)/wallpapers"
+
+[[ -d "$wallpaper_dir" ]] || exit 1
+
 wallpapers=("$wallpaper_dir"/*)
 total_wallpapers=${#wallpapers[@]}
-random_index=$((RANDOM % "$total_wallpapers"))
-wallpaper=${wallpapers["$random_index"]}
-bg_info=$(awww query)
-current_wallpaper="${bg_info##*:}"
-awww img "$wallpaper" --transition-fps 60 --transition-type wipe --transition-duration 3
+
+[[ "$total_wallpapers" -eq 0 ]] && exit 1
+
+random_index=$((RANDOM % total_wallpapers))
+wallpaper="${wallpapers[$random_index]}"
+
+bg_info=$(awww query 2>/dev/null) || exit 1
+current_wallpaper=$(basename "${bg_info##*:}")
+
+if [[ "$(basename "$wallpaper")" != "$current_wallpaper" ]]; then
+	awww img "$wallpaper" --transition-fps 60 --transition-type wipe --transition-duration 3
+fi
